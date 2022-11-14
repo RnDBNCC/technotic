35c2917c6e93c9020c3e91150368ded9d402a53a
joshuajevon <joshuajevon05@gmail.com>
2022-11-14 15:50:35 +0700

    added navbar

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -3,7 +3,9 @@ import React from "react";
 const navbar = () => {
     return (
         <div>
-            
+            <a href="">Testing1</a>
+            <a href="">Testing2</a>
+            <a href="">Testing3</a>
         </div>
     );
   };
