f762bee37870757c0772cca3a10ef54e5deb6052
joshuajevon <joshuajevon05@gmail.com>
2022-11-16 21:34:35 +0700

    feat(navbar):added navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,14 +1,37 @@
-import React from "react";
+import React from 'react';
+import { navbarContainerStyle } from './styles';
+import { Person } from './types';
+import { cx } from '@emotion/css';
 
-const Navbar = () => {
-    return(
-      <div>
-            <a href="">Testing1</a>
-            <a href="">Testing2</a>
-            <a href="">Testing3</a>
-        </div>
-    ); 
-  };
+interface NavbarProps {
+  link: number;
+  link2?: string;
+  person: Person[];
+}
 
+type NavbarTechnotic = (props: NavbarProps) => JSX.Element;
+
+const Navbar: NavbarTechnotic = ({ link2 }) => {
+  return (
+    <div className={cx(navbarContainerStyle)}>
+      <a href={link2}>Testing1</a>
+      <a href="">Testing2</a>
+      <a href="">Testing3</a>
+    </div>
+  );
+};
+
+const Testing = () => {
+  return (
+    <Navbar
+      person={[
+        { name: 'Rifian', age: 18 },
+        { name: 'Joce', age: 50 },
+      ]}
+      link2="https://www.youtube.com"
+      link={1234}
+    />
+  );
+};
 
 export default Navbar;
