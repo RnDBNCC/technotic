f90ce9aad0042b382e16792b015f22ac36b9fa09
joshuajevon <joshuajevon05@gmail.com>
2022-11-16 20:33:29 +0700

    added component

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+    return(
+      <div>
+            <a href="">Testing1</a>
+            <a href="">Testing2</a>
+            <a href="">Testing3</a>
+        </div>
+    ); 
+  };
+
+
+export default Navbar;
--- src/components/NewComponent/index.tsx
+++ src/components/NewComponent/index.tsx
@@ -2,13 +2,7 @@ import React from 'react';
 
 const NewComponent = () => {
   return (
-    <>NewComponent</>,
-    <h1>test</h1>,
-    <div>
-      <a href="">Testing1</a>
-      <a href="">Testing2</a>
-      <a href="">Testing3</a>
-    </div>
+    <>NewComponent</>
   );
 };
 
