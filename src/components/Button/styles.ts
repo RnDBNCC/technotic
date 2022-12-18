fcf2aaeaa7fccb8561d2fd984e9429473a9551b7
RFJST4REAL <90315898+RFJST4REAL@users.noreply.github.com>
2022-12-19 01:40:15 +0700

    feat(Button): new branch & button feat

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,13 @@
+import { css } from '@emotion/css';
+
+export const ButtonStyle = css({
+    backgroundColor: '#0EB0DD',
+    padding: '1rem 1.5rem 1rem 1.5rem',
+    borderRadius: '1rem',
+});
+
+// export const ButtonStyle = css({
+//     backgroundColor: '#0EB0DD',
+//     padding: '1rem 1.5rem 1rem 1.5rem',
+//     borderRadius: '1rem'
+// })
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { default as NewComponent } from './components/NewComponent';
 export { default as Navbar } from './components/Navbar';
+export { default as Button } from './components/Button';
