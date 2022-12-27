d9b4309f37beda2123d1724187f35c86a76dd1fd
joshua jevon irawan <joshuajevon05@gmail.com>
2022-12-27 23:13:04 +0700

    feat(Input.TextField): added html-input-element and added icon

--- src/components/Input/TextField/TextFieldProps.ts
+++ src/components/Input/TextField/TextFieldProps.ts
@@ -1,14 +1,14 @@
 import { ChangeEvent, FormEvent } from 'react';
 
-export interface TextFieldProps {
+type SizeType = 'small' | 'medium' | 'large' | undefined;
+
+export interface TextFieldProps extends HTMLInputElement {
+    inputSize?: SizeType;
     textLabel?: string;
-    padding?: string;
     fontSize?: string;
     borderRadius?: string;
     icon?: string;
-    display?: string;
-    placeholder?: string;
-    width?: string;
+    textPlaceholder?: string;
 
     onInputChange?: (e: ChangeEvent<HTMLInputElement>) => void;
     onSubmit?: (e: FormEvent) => void;
