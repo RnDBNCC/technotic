35acad4a5d704a94d8390b28bafbf800295cdb94
arraa <tiararahmah82@gmail.com>
2022-12-28 23:22:32 +0700

    feat(input.Radio): new branch and radio feat

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { default as NewComponent } from './components/NewComponent';
 export { default as Navbar } from './components/Navbar';
+export { default as Radio } from './components/Input/Radio';
