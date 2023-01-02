423cc6cbcbeb96f9f737f7d0f4bb83da9f20623a
RFJST4REAL <rifianfernando19@gmail.com>
2023-01-02 17:25:27 +0700

    feat(button): link href pathname 50% done

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,19 +1,27 @@
 import React, { PropsWithChildren } from 'react';
 import { styButtonPrimary, styUndefinedBtn } from './styles';
 import { ButtonProps } from './buttonProps';
+import { isValidHttpUrl, isPath } from './Route';
 import { cx } from '@emotion/css';
 
 const Button = (props: PropsWithChildren<ButtonProps>): JSX.Element => {
-    const { btnSize, btntype, btnColor, to, children, ...rest } = props;
+    const { btnSize, btntype, btnColor, link, element, children, ...rest } =
+        props;
     const className =
         btnSize !== undefined
             ? `${btnSize} ${styButtonPrimary}`
             : cx(styButtonPrimary, styUndefinedBtn);
     const color = { backgroundColor: btnColor };
+
+    function handleClick(): void {
+        if (link !== undefined) isValidHttpUrl(link);
+        if (link !== undefined && element !== undefined) isPath(link, element);
+    }
+
     return (
         <div>
             <button
-                onClick={() => {}}
+                onClick={handleClick}
                 {...rest}
                 style={color}
                 className={className}
