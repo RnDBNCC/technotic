5ce1355953dbadef0ee103a13ad55feac8ac4243
JonathanChr11 <jonathansoesanto@gmail.com>
2023-01-26 12:59:13 +0700

    feat(Typography & DatePicker): Typography func + design done, DatePicker error & success state done

--- src/components/DatePicker/index.tsx
+++ src/components/DatePicker/index.tsx
@@ -11,6 +11,8 @@ const DatePicker = (props: PropsWithChildren<DatePickerProps>): JSX.Element => {
     const datePickerStatus =
         status !== undefined ? `${status} ${styDatePicker}` : cx(styDatePicker);
 
+    console.log(rest);
+
     return (
         <div className={cx(styLayout)}>
             <input
