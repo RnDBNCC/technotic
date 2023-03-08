1cd1f0d7436552cfb6e0bb331ff23778e46044cc
RFJST4REAL <rifianfernando19@gmail.com>
2023-03-08 21:27:52 +0700

    feat(commit): add rules commit

---
+++ commitlint.config.cjs
@@ -0,0 +1,24 @@
+// https://commitlint.js.org/#/reference-rules
+// https://yarnpkg.com/package/@commitlint/config-conventional
+
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [
+            2,
+            'always',
+            [
+                'feat',
+                'fix',
+                'docs',
+                'style',
+                'refactor',
+                'test',
+                'chore',
+                'revert',
+            ],
+        ],
+        'subject-full-stop': [0, 'never'],
+        'subject-case': [0, 'never'],
+    },
+};
