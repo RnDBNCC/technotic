9f92354fcb744abcdb8a8b881dfc484d4eac6ccd
RFJST4REAL <rifianfernando19@gmail.com>
2023-03-22 21:45:04 +0700

    feat(button): add link button for button feature

--- src/Utils/httpValidation.ts
+++ src/Utils/httpValidation.ts
@@ -1,6 +1,6 @@
 export function Route(path: string, children: any): void {
     window.history.pushState({}, '', path); // update pathname
-
+    console.log(window.location.pathname);
     // kasih tau routenya udh keubah
     const navEvent = new PopStateEvent('popstate');
     window.dispatchEvent(navEvent);
@@ -9,8 +9,12 @@ export function Route(path: string, children: any): void {
 }
 
 export function isValidHttpUrl(link: string): boolean {
-    const url: any = new URL(link);
-    window.location.href = url;
+    if (link.includes('http')) {
+        const url: any = new URL(link);
+        window.location.href = url;
+    }
+    // add link to wiindow.location.href
+    window.location.href = window.location.href + link;
 
     return true;
 }
