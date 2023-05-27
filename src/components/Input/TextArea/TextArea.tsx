dd70e223c065c6b3b37fcaa283ad5cb92f490f3e
DefarioFitriano <defariofitriano@gmail.com>
2023-05-28 01:06:33 +0700

    fix: fixed style

---
+++ src/components/Input/TextArea/TextArea.tsx
@@ -0,0 +1,31 @@
+import { cx } from '@emotion/css';
+import React, { PropsWithChildren } from 'react';
+import { TextAreaProps } from './TextAreaProps';
+import { textAreaStyle, stySizeDefault, styStatusDefault } from './styles';
+
+const TextArea = (props: PropsWithChildren<TextAreaProps>): JSX.Element => {
+    const { placeholder, text, txtAreaSize, txtAreaStatus } = props;
+
+    const inputSize =
+        txtAreaSize !== undefined
+            ? `${txtAreaSize} ${textAreaStyle}`
+            : cx(stySizeDefault);
+
+    const inputStatus =
+        txtAreaStatus !== undefined
+            ? `${txtAreaStatus} ${textAreaStyle}`
+            : cx(styStatusDefault);
+
+    return (
+        <div>
+            <textarea
+                className={`${inputSize} ${inputStatus}`}
+                placeholder={placeholder}
+            >
+                {text}
+            </textarea>
+        </div>
+    );
+};
+
+export default TextArea;
