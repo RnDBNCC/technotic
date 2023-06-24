4081a99673b639c6664c971384a623f84288abdf
JonathanChr11 <90307788+JonathanChr11@users.noreply.github.com>
2023-06-24 23:36:43 +0700

    fix: ON and OFF Text in Toggle Button Switch (#70)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Randi Adiel Gianufian <randiadiel30@gmail.com>
    Co-authored-by: Automated Version Bump <gh-action-bump-version@users.noreply.github.com>

--- src/components/Switch/style.ts
+++ src/components/Switch/style.ts
@@ -10,6 +10,7 @@ export const styleSwitch = css`
     //default
     --width: 70px;
     --height: calc(var(--width) / 2.5);
+    --primary: #22539f;
 
     position: relative;
     display: inline-block;
@@ -34,7 +35,7 @@ export const styleSwitch = css`
             left: 0;
             width: calc(var(--height));
             height: calc(var(--height));
-            background-color: #F7F7F7;
+            background-color: #f7f7f7;
             border-radius: var(--height);
             transition: 0.3s all ease-in-out;
         }
@@ -45,20 +46,20 @@ export const styleSwitch = css`
     }
 
     input:checked + .slider {
-        background-color: #22539F;
+        background-color: var(--primary);
 
         &::before {
-            transform: translateX(calc(var(--width) - var(--height)))
+            transform: translateX(calc(var(--width) - var(--height)));
         }
     }
 
     .labels {
         position: absolute;
-        top: 8px;
-        left: 0:
+        top: 3.5px;
+        left: 0;
         width: 100%;
         height: 100%;
-        font-size: 16px;
+        font-size: 14px;
         transition: 0.3s all ease-in-out;
         overflow: hidden;
 
@@ -66,7 +67,7 @@ export const styleSwitch = css`
             content: attr(data-off);
             position: absolute;
             color: #fff;
-            left: 5px;
+            right: 5px;
             opacity: 1;
             transition: 0.3s all ease-in-out;
         }
@@ -75,7 +76,7 @@ export const styleSwitch = css`
             content: attr(data-on);
             position: absolute;
             color: #fff;
-            right: 5px;
+            left: calc(var(--width) / -2);
             opacity: 0;
             transition: 0.3s all ease-in-out;
         }
@@ -83,17 +84,22 @@ export const styleSwitch = css`
 
     input:checked ~ .labels::after {
         opacity: 0;
-        transform: translateX(calc(var(--width) - var(--height)))
+        transform: translateX(calc(var(--width) - var(--height)));
     }
 
     input:checked ~ .labels::before {
         opacity: 1;
-        transform: translateX(calc(var(--width) - var(--height)))
+        transform: translateX(calc(var(--width) - var(--height)));
     }
 
     &.small {
         --width: 40px;
         --height: calc(var(--width) / 2.5);
+
+        .labels {
+            top: 3px;
+            font-size: 8px;
+        }
     }
 
     &.medium {
@@ -104,5 +110,10 @@ export const styleSwitch = css`
     &.large {
         --width: 100px;
         --height: calc(var(--width) / 2.5);
+
+        .labels {
+            top: 6px;
+            font-size: 18px;
+        }
     }
 `;
