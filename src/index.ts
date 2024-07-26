b69f123f951209c6f95a1fddc8d69e9c6de896bc
unknown <[aurelius.bd123@gmail.com]>
2024-07-26 12:37:58 +0700

    feat(accordion): add toggle accordion functionality

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-export { default as NewComponent } from './components/NewComponent';
 export { default as Navbar } from './components/Navbar';
 export { default as ColorSystem } from './components/ColorSystem';
 export { default as Button } from './components/Button/index';
@@ -6,7 +5,7 @@ export { default as Input } from './components/Input';
 export { default as Typography } from './components/Typography';
 export { default as DatePicker } from './components/DatePicker';
 export { default as Switch } from './components/Switch';
+export { default as Accordion } from './components/Accordion';
 export { default as Footer } from './components/Footer';
-
 export { TypographyProps } from './components/Typography/TypographyProps';
 export { DatePickerProps } from './components/DatePicker/DatePickerProps';
